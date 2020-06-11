26e4491896643f4a659afa12fc20f4fdd8569687
Yulia Karpova <karpova.jul@gmail.com>
2020-06-11 18:24:14 +1000

    1.4 Добавит пропсы в компоненты Main и App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Main from "../main/main.jsx";
 
-const App = () => {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {offersCount} = props;
+
   return (
-    <Main />
+    <Main offersCount={offersCount}/>
   );
 };
 
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-const Main = () => {
+const Main = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {offersCount} = props;
+
   return (
     <div className="page page--gray page--main">
       <header className="header">
@@ -68,7 +71,7 @@ const Main = () => {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
+              <b className="places__found">{offersCount} places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex="0">
