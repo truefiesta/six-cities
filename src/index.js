6a0a49f72bb672ce201996f6ec1b6df7f1193500
Yulia Karpova <karpova.jul@gmail.com>
2020-06-13 15:31:10 +1000

    2.1 Установит prop-types, передаст названия предложений в пропсах и добавит валидацию для компонент

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ const Settings = {
   OFFERS_COUNT: 312
 };
 
+const offerTitles = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+  `Wood and stone place`
+];
+
 ReactDOM.render(
     <App
       offersCount={Settings.OFFERS_COUNT}
+      offerTitles={offerTitles}
     />,
     document.querySelector(`#root`)
 );
