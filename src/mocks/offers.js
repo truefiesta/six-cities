633b64ddebb02310f8aefac714e5059807733abc
Yulia Karpova <karpova.jul@gmail.com>
2020-06-20 10:34:21 +1000

    3.1 Опишет структуру данных для карточки предложения

---
+++ src/mocks/offers.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    image: `http://lorempixel.com/400/200/`,
+    price: 120,
+    rating: 80,
+    name: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    isBookmarked: false,
+    isPremium: true,
+  },
+  {
+    image: `http://lorempixel.com/400/200/`,
+    price: 80,
+    rating: 80,
+    name: `Wood and stone place`,
+    type: `Private room`,
+    isBookmarked: true,
+    isPremium: false,
+  },
+  {
+    image: `http://lorempixel.com/400/200/`,
+    price: 132,
+    rating: 80,
+    name: `Canal view Prinsengracht`,
+    type: `Apartment`,
+    isBookmarked: false,
+    isPremium: false,
+  },
+  {
+    image: `http://lorempixel.com/400/200/`,
+    price: 180,
+    rating: 100,
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    isBookmarked: false,
+    isPremium: true,
+  }
+];
