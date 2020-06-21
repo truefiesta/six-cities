7ad959f84ec251b1b1f4333e3b1b2a9a05f6b947
Yulia Karpova <karpova.jul@gmail.com>
2020-06-21 10:05:37 +1000

    3.9 Превратит App в класс-компонент, подключит react-router-dom и настроит роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main.jsx";
+import OfferDetails from "../offer-details/offer-details.jsx";
 
 const onOfferDetailsOpen = () => {};
 
-const App = (props) => {
-  const {offersCount, offers} = props;
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <Main
-      offersCount={offersCount}
-      offers={offers}
-      onOfferDetailsOpen={onOfferDetailsOpen}
-    />
-  );
-};
+  _renderApp() {
+    const {offersCount, offers} = this.props;
+
+    return (
+      <Main
+        offersCount={offersCount}
+        offers={offers}
+        onOfferDetailsOpen={onOfferDetailsOpen}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-offer-details">
+            <OfferDetails />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+}
 
 App.propTypes = {
   offersCount: PropTypes.number.isRequired,
