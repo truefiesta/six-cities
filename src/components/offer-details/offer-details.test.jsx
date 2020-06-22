c57f3164149759987527102eb89ad23063995678
Yulia Karpova <karpova.jul@gmail.com>
2020-06-22 12:38:46 +1000

    3.13 Создаст снепшот-тест для компонента «Детальная информация о предложении»

---
+++ src/components/offer-details/offer-details.test.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import OfferDetails from "./offer-details.jsx";
+
+const offer = {
+  id: `offer-1`,
+  image: `http://lorempixel.com/400/200/`,
+  photos: [
+    {
+      src: `img/room.jpg`,
+      alt: `Photo studio`,
+    },
+    {
+      src: `img/apartment-01.jpg`,
+      alt: `Photo studio`,
+    },
+    {
+      src: `img/apartment-02.jpg`,
+      alt: `Photo studio`,
+    },
+    {
+      src: `img/apartment-03.jpg`,
+      alt: `Photo studio`,
+    },
+    {
+      src: `img/studio-01.jpg`,
+      alt: `Photo studio`,
+    },
+    {
+      src: `img/apartment-01.jpg`,
+      alt: `Photo studio`,
+    },
+    {
+      src: `img/room.jpg`,
+      alt: `Photo studio`,
+    },
+  ],
+  price: 120,
+  rating: 4.1,
+  name: `Beautiful & luxurious apartment at great location`,
+  description: [
+    `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+    `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
+  ],
+  bedrooms: 3,
+  guests: 4,
+  equipment: [`Wi-Fi`, `Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Coffee machine`, `Dishwasher`, `Towels`, `Baby seat`, `Cabel TV`],
+  type: `apartment`,
+  host: {
+    avatar: `img/avatar-angelina.jpg`,
+    name: `Angelina`,
+    status: `super`,
+  },
+  isBookmarked: false,
+  isPremium: true,
+};
+
+describe(`src/offers-details.jsx`, () => {
+  describe(`when the offers is a non-empty array`, () => {
+    it(`should render with data`, () => {
+      const tree = renderer.create(
+          <OfferDetails
+            offer={offer}
+          />
+      )
+      .toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
