e65cdd501d89bf5ec49cf92119c3ea64072b293f
Yulia Karpova <karpova.jul@gmail.com>
2020-06-29 20:37:04 +1000

    4.14  Создаст тестовые моки и импортирует их в тесты, обновит снепшоты

---
+++ src/test-mocks/test-mocks.js
@@ -0,0 +1,392 @@
+export default [
+  {
+    id: `offer-1`,
+    image: `http://lorempixel.com/400/200/`,
+    photos: [
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-02.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-03.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/studio-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+    ],
+    price: 120,
+    rating: 4.1,
+    city: `Amsterdam`,
+    coordinates: [52.3909553943508, 4.85309666406198],
+    name: `Beautiful & luxurious apartment at great location`,
+    description: [
+      `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+      `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
+    ],
+    bedrooms: 3,
+    guests: 4,
+    equipment: [`Wi-Fi`, `Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Coffee machine`, `Dishwasher`, `Towels`, `Baby seat`, `Cabel TV`],
+    type: `apartment`,
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angelina`,
+      status: `super`,
+    },
+    isBookmarked: false,
+    isPremium: true,
+    reviews: [
+      {
+        id: `r000001`,
+        text: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+        rating: 4.1,
+        user: {
+          name: `Max`,
+        },
+        date: `2019-04-24`,
+      },
+    ],
+  },
+  {
+    id: `offer-2`,
+    image: `http://lorempixel.com/400/200/`,
+    photos: [
+      {
+        src: `img/apartment-03.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-02.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-03.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/studio-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+    ],
+    price: 80,
+    rating: 4.2,
+    city: `Amsterdam`,
+    coordinates: [52.369553943508, 4.85309666406198],
+    name: `Wood and stone place`,
+    description: [
+      `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+      `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
+    ],
+    bedrooms: 2,
+    guests: 4,
+    equipment: [`Wi-Fi`, `Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Coffee machine`, `Dishwasher`, `Towels`, `Baby seat`, `Cabel TV`],
+    type: `room`,
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angelina`,
+      status: `super`,
+    },
+    isBookmarked: true,
+    isPremium: false,
+    reviews: [
+      {
+        id: `r000003`,
+        text: `Styled to complement the original features in the building, this historic warehouse is centrally located in the cool and vibrant city center.`,
+        rating: 4.1,
+        user: {
+          name: `George`,
+        },
+        date: `2019-04-24`,
+      },
+      {
+        id: `r000004`,
+        text: `Beautiful apartment in a fantastic location. Had a great stay!`,
+        rating: 5,
+        user: {
+          name: `Taylor`,
+        },
+        date: `2019-03-17`,
+      },
+    ],
+  },
+  {
+    id: `offer-3`,
+    image: `http://lorempixel.com/400/200/`,
+    photos: [
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-02.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-03.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/studio-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+    ],
+    price: 132,
+    rating: 4.3,
+    city: `Amsterdam`,
+    coordinates: [52.3909553943508, 4.929309666406198],
+    name: `Canal view Prinsengracht`,
+    description: [
+      `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+      `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
+    ],
+    bedrooms: 3,
+    guests: 4,
+    equipment: [`Wi-Fi`, `Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Coffee machine`, `Dishwasher`, `Towels`, `Baby seat`, `Cabel TV`],
+    type: `apartment`,
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angelina`,
+      status: `super`,
+    },
+    isBookmarked: false,
+    isPremium: false,
+    reviews: [
+      {
+        id: `r000005`,
+        text: `After experiencing dreadful customer service previously, I vowed not to return, however we decided to give it another try as it is one of our locals. We are glad we did.`,
+        rating: 4.5,
+        user: {
+          name: `Philipe`,
+        },
+        date: `2019-05-01`,
+      },
+      {
+        id: `r000006`,
+        text: `A hidden city joy. Apartment that hides, because it is not actually in Collins street but tucked around the corner in a lane.`,
+        rating: 4.2,
+        user: {
+          name: `Sharyn`,
+        },
+        date: `2019-03-17`,
+      },
+      {
+        id: `r000007`,
+        text: `Located on the first floor this 17 foot high loft apartment is a design sanctuary. Centrally located to the city.`,
+        rating: 5.0,
+        user: {
+          name: `Dani`,
+        },
+        date: `2019-06-14`,
+      },
+    ],
+  },
+  {
+    id: `offer-4`,
+    image: `http://lorempixel.com/400/200/`,
+    photos: [
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-02.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-03.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/studio-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+    ],
+    price: 180,
+    rating: 5,
+    city: `Amsterdam`,
+    coordinates: [52.3809553943508, 4.939309666406198],
+    name: `Nice, cozy, warm big bed apartment`,
+    description: [
+      `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+      `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
+    ],
+    bedrooms: 2,
+    guests: 4,
+    equipment: [`Wi-Fi`, `Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Coffee machine`, `Dishwasher`, `Towels`, `Baby seat`, `Cabel TV`],
+    type: `apartment`,
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angelina`,
+      status: `super`,
+    },
+    isBookmarked: false,
+    isPremium: true,
+    reviews: [
+      {
+        id: `r000008`,
+        text: `Very good location ,lovely staff ,clean apartment, great beds.`,
+        rating: 4.8,
+        user: {
+          name: `Anthony`,
+        },
+        date: `2019-06-25`,
+      },
+      {
+        id: `r000002`,
+        text: `Neighbours were noisy until the early hours.`,
+        rating: 3.8,
+        user: {
+          name: `Jerry`,
+        },
+        date: `2019-05-12`,
+      },
+      {
+        id: `r000009`,
+        text: `Excellent location. Apartment well laid out and very roomy.`,
+        rating: 4.2,
+        user: {
+          name: `Thomas`,
+        },
+        date: `2019-06-12`,
+      },
+      {
+        id: `r000010`,
+        text: `Location was fantastic, Amenities were good, views were good, would probably stay again. Nice large comfortable penthouse with outside areas to sit. Lounge was comfortable and kitchen had what was needed. Close to shops and supermarkets.`,
+        rating: 5.0,
+        user: {
+          name: `Larry`,
+        },
+        date: `2019-06-10`,
+      },
+      {
+        id: `r000011`,
+        text: `Very spacious, clean and in a perfect location.`,
+        rating: 4.9,
+        user: {
+          name: `Kim`,
+        },
+        date: `2019-05-11`,
+      },
+      {
+        id: `r000012`,
+        text: `Nice, but also the check in was very confusing....especially where the office is actually located......putting the notice on the door might work better than on the window where you’re not looking.`,
+        rating: 4.6,
+        user: {
+          name: `Holy`,
+        },
+        date: `2019-05-05`,
+      },
+      {
+        id: `r000013`,
+        text: `The master bed room is spacious and there should be an additional chair and a desk. the last bed room closet need hangers.`,
+        rating: 4.8,
+        user: {
+          name: `Dylan`,
+        },
+        date: `2019-06-21`,
+      },
+      {
+        id: `r000014`,
+        text: `There was nothing I didn't like, it was far better than what I expected!!`,
+        rating: 5.0,
+        user: {
+          name: `Bernadette`,
+        },
+        date: `2019-05-22`,
+      },
+      {
+        id: `r000015`,
+        text: `Excellent location. Apartment well laid out and very roomy.`,
+        rating: 4.2,
+        user: {
+          name: `Tanya`,
+        },
+        date: `2019-04-11`,
+      },
+      {
+        id: `r000016`,
+        text: `No major issues just minor ones of the TV was not a smart TV, so it could connect to our devices wireless and we had to use a cable to stream things from our laptop.`,
+        rating: 4.4,
+        user: {
+          name: `Caroline`,
+        },
+        date: `2019-03-10`,
+      },
+      {
+        id: `r000017`,
+        text: `Very spacious, clean and in a perfect location.`,
+        rating: 4.9,
+        user: {
+          name: `Sandra`,
+        },
+        date: `2019-05-11`,
+      },
+      {
+        id: `r000018`,
+        text: `Great if you want to stay up talking and others do to bed.`,
+        rating: 4.6,
+        user: {
+          name: `Amanda`,
+        },
+        date: `2019-05-07`,
+      },
+    ],
+  }
+];
