8605453e92e57984fb2e7123caaaafdb47492baf
Yulia Karpova <karpova.jul@gmail.com>
2020-06-29 20:07:04 +1000

    4.12 Обновит моки в e2e-тесте компонента App и в снепшот-тесте компонента OfferDetails

--- src/components/app/app.e2e.test.js
+++ src/components/app/app.e2e.test.js
@@ -65,6 +65,17 @@ const offers = [
     },
     isBookmarked: false,
     isPremium: true,
+    reviews: [
+      {
+        id: `r000001`,
+        text: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+        rating: 4.1,
+        user: {
+          name: `Max`,
+        },
+        date: `2019-04-24`,
+      },
+    ],
   },
   {
     id: `offer-2`,
@@ -123,6 +134,26 @@ const offers = [
     },
     isBookmarked: true,
     isPremium: false,
+    reviews: [
+      {
+        id: `r000003`,
+        text: `Styled to complement the original features in the building, this historic warehouse is centrally located in the cool and vibrant city center.`,
+        rating: 4.1,
+        user: {
+          name: `George`,
+        },
+        date: `2019-04-24`,
+      },
+      {
+        id: `r000004`,
+        text: `Beautiful apartment in a fantastic location. Had a great stay!`,
+        rating: 5,
+        user: {
+          name: `Taylor`,
+        },
+        date: `2019-03-17`,
+      },
+    ],
   },
   {
     id: `offer-3`,
@@ -177,6 +208,35 @@ const offers = [
     },
     isBookmarked: false,
     isPremium: false,
+    reviews: [
+      {
+        id: `r000005`,
+        text: `After experiencing dreadful customer service previously, I vowed not to return, however we decided to give it another try as it is one of our locals. We are glad we did.`,
+        rating: 4.5,
+        user: {
+          name: `Philipe`,
+        },
+        date: `2019-05-01`,
+      },
+      {
+        id: `r000006`,
+        text: `A hidden city joy. Apartment that hides, because it is not actually in Collins street but tucked around the corner in a lane.`,
+        rating: 4.2,
+        user: {
+          name: `Sharyn`,
+        },
+        date: `2019-03-17`,
+      },
+      {
+        id: `r000007`,
+        text: `Located on the first floor this 17 foot high loft apartment is a design sanctuary. Centrally located to the city.`,
+        rating: 5.0,
+        user: {
+          name: `Dani`,
+        },
+        date: `2019-06-14`,
+      },
+    ],
   },
   {
     id: `offer-4`,
@@ -231,6 +291,116 @@ const offers = [
     },
     isBookmarked: false,
     isPremium: true,
+    reviews: [
+      {
+        id: `r000008`,
+        text: `Very good location ,lovely staff ,clean apartment, great beds.`,
+        rating: 4.8,
+        user: {
+          name: `Anthony`,
+        },
+        date: `2019-06-25`,
+      },
+      {
+        id: `r000002`,
+        text: `Neighbours were noisy until the early hours.`,
+        rating: 3.8,
+        user: {
+          name: `Jerry`,
+        },
+        date: `2019-05-12`,
+      },
+      {
+        id: `r000009`,
+        text: `Excellent location. Apartment well laid out and very roomy.`,
+        rating: 4.2,
+        user: {
+          name: `Thomas`,
+        },
+        date: `2019-06-12`,
+      },
+      {
+        id: `r000010`,
+        text: `Location was fantastic, Amenities were good, views were good, would probably stay again. Nice large comfortable penthouse with outside areas to sit. Lounge was comfortable and kitchen had what was needed. Close to shops and supermarkets.`,
+        rating: 5.0,
+        user: {
+          name: `Larry`,
+        },
+        date: `2019-06-10`,
+      },
+      {
+        id: `r000011`,
+        text: `Very spacious, clean and in a perfect location.`,
+        rating: 4.9,
+        user: {
+          name: `Kim`,
+        },
+        date: `2019-05-11`,
+      },
+      {
+        id: `r000012`,
+        text: `Nice, but also the check in was very confusing....especially where the office is actually located......putting the notice on the door might work better than on the window where you’re not looking.`,
+        rating: 4.6,
+        user: {
+          name: `Holy`,
+        },
+        date: `2019-05-05`,
+      },
+      {
+        id: `r000013`,
+        text: `The master bed room is spacious and there should be an additional chair and a desk. the last bed room closet need hangers.`,
+        rating: 4.8,
+        user: {
+          name: `Dylan`,
+        },
+        date: `2019-06-21`,
+      },
+      {
+        id: `r000014`,
+        text: `There was nothing I didn't like, it was far better than what I expected!!`,
+        rating: 5.0,
+        user: {
+          name: `Bernadette`,
+        },
+        date: `2019-05-22`,
+      },
+      {
+        id: `r000015`,
+        text: `Excellent location. Apartment well laid out and very roomy.`,
+        rating: 4.2,
+        user: {
+          name: `Tanya`,
+        },
+        date: `2019-04-11`,
+      },
+      {
+        id: `r000016`,
+        text: `No major issues just minor ones of the TV was not a smart TV, so it could connect to our devices wireless and we had to use a cable to stream things from our laptop.`,
+        rating: 4.4,
+        user: {
+          name: `Caroline`,
+        },
+        date: `2019-03-10`,
+      },
+      {
+        id: `r000017`,
+        text: `Very spacious, clean and in a perfect location.`,
+        rating: 4.9,
+        user: {
+          name: `Sandra`,
+        },
+        date: `2019-05-11`,
+      },
+      {
+        id: `r000018`,
+        text: `Great if you want to stay up talking and others do to bed.`,
+        rating: 4.6,
+        user: {
+          name: `Amanda`,
+        },
+        date: `2019-05-07`,
+      },
+    ],
   }
 ];
 
--- src/components/offer-details/offer-details.test.jsx
+++ src/components/offer-details/offer-details.test.jsx
@@ -53,7 +53,47 @@ const offer = {
   },
   isBookmarked: false,
   isPremium: true,
+  reviews: [
+    {
+      id: `r000001`,
+      text: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+      rating: 4.1,
+      user: {
+        name: `Max`,
+      },
+      date: `2019-04-24`,
+    },
+  ],
 };
+
+jest.mock(`../reviews-list/reviews-list.jsx`, () => () => {
+  return (
+    <ul className="reviews__list">
+      <li className="reviews__item">
+        <div className="reviews__user user">
+          <div className="reviews__avatar-wrapper user__avatar-wrapper">
+            <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
+          </div>
+          <span className="reviews__user-name">
+            Max
+          </span>
+        </div>
+        <div className="reviews__info">
+          <div className="reviews__rating rating">
+            <div className="reviews__stars rating__stars">
+              <span style={{width: `80%`}}></span>
+              <span className="visually-hidden">Rating</span>
+            </div>
+          </div>
+          <p className="reviews__text">
+            A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
+          </p>
+          <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
+        </div>
+      </li>
+    </ul>
+  );
+});
 
 describe(`src/offers-details.jsx`, () => {
   describe(`when the offers is a non-empty array`, () => {
