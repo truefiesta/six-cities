c39cda5e16f8ab6deec70ea5870842f5de88379d
Yulia Karpova <karpova.jul@gmail.com>
2020-07-07 23:56:55 +1000

    5.5 Создаст компонент «Список городов»

---
+++ src/components/cities-list/cities-list.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {ActionCreator} from "../../reducer";
+
+const ACTIVE_CLASS = `tabs__item--active`;
+
+const CitiesList = ({city, cities, onCityClick}) => {
+  const sixCities = cities.slice(0, 6);
+
+  return (
+    <ul className="locations__list tabs__list">
+      {sixCities.map((it) => {
+        return (
+          <li key={it} className="locations__item">
+            <a
+              onClick={(evt) => {
+                evt.preventDefault();
+                onCityClick(it);
+              }}
+              className={`locations__item-link tabs__item ${city === it ? ACTIVE_CLASS : `` }`}
+              href="#"
+            >
+              <span>{it}</span>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+CitiesList.propTypes = {
+  city: PropTypes.string.isRequired,
+  cities: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onCityClick: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  onCityClick(city) {
+    dispatch(ActionCreator.changeCity(city));
+  }
+});
+
+export {CitiesList};
+export default connect(null, mapDispatchToProps)(CitiesList);
+
