9b4559da657dca55600a1422265dcd6aa465f10b
Yulia Karpova <karpova.jul@gmail.com>
2020-07-08 11:26:57 +1000

    5.11 Добавит в тестовые моки предложение в Paris

--- src/test-mocks/test-mocks.js
+++ src/test-mocks/test-mocks.js
@@ -400,5 +400,172 @@ export default [
     offersNearbyIds: [
       `offer-2`, `offer-3`, `offer-1`
     ],
+  },
+  {
+    id: `offer-5`,
+    image: `img/apartment-01.jpg`,
+    photos: [
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-02.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-03.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/studio-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/apartment-01.jpg`,
+        alt: `Photo studio`,
+      },
+      {
+        src: `img/room.jpg`,
+        alt: `Photo studio`,
+      },
+    ],
+    price: 180,
+    rating: 5,
+    city: `Paris`,
+    coordinates: [48.8647163943578, 2.359309666406199],
+    name: `Nice, cozy, warm big bed apartment`,
+    description: [
+      `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+      `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`
+    ],
+    bedrooms: 2,
+    guests: 4,
+    equipment: [`Wi-Fi`, `Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Coffee machine`, `Dishwasher`, `Towels`, `Baby seat`, `Cabel TV`],
+    type: `apartment`,
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Angelina`,
+      status: `super`,
+    },
+    isBookmarked: false,
+    isPremium: true,
+    reviews: [
+      {
+        id: `r000008`,
+        text: `Very good location ,lovely staff ,clean apartment, great beds.`,
+        rating: 4.8,
+        user: {
+          name: `Anthony`,
+        },
+        date: `2019-06-25`,
+      },
+      {
+        id: `r000002`,
+        text: `Neighbours were noisy until the early hours.`,
+        rating: 3.8,
+        user: {
+          name: `Jerry`,
+        },
+        date: `2019-05-12`,
+      },
+      {
+        id: `r000009`,
+        text: `Excellent location. Apartment well laid out and very roomy.`,
+        rating: 4.2,
+        user: {
+          name: `Thomas`,
+        },
+        date: `2019-06-12`,
+      },
+      {
+        id: `r000010`,
+        text: `Location was fantastic, Amenities were good, views were good, would probably stay again. Nice large comfortable penthouse with outside areas to sit. Lounge was comfortable and kitchen had what was needed. Close to shops and supermarkets.`,
+        rating: 5.0,
+        user: {
+          name: `Larry`,
+        },
+        date: `2019-06-10`,
+      },
+      {
+        id: `r000011`,
+        text: `Very spacious, clean and in a perfect location.`,
+        rating: 4.9,
+        user: {
+          name: `Kim`,
+        },
+        date: `2019-05-11`,
+      },
+      {
+        id: `r000012`,
+        text: `Nice, but also the check in was very confusing....especially where the office is actually located......putting the notice on the door might work better than on the window where you’re not looking.`,
+        rating: 4.6,
+        user: {
+          name: `Holy`,
+        },
+        date: `2019-05-05`,
+      },
+      {
+        id: `r000013`,
+        text: `The master bed room is spacious and there should be an additional chair and a desk. the last bed room closet need hangers.`,
+        rating: 4.8,
+        user: {
+          name: `Dylan`,
+        },
+        date: `2019-06-21`,
+      },
+      {
+        id: `r000014`,
+        text: `There was nothing I didn't like, it was far better than what I expected!!`,
+        rating: 5.0,
+        user: {
+          name: `Bernadette`,
+        },
+        date: `2019-05-22`,
+      },
+      {
+        id: `r000015`,
+        text: `Excellent location. Apartment well laid out and very roomy.`,
+        rating: 4.2,
+        user: {
+          name: `Tanya`,
+        },
+        date: `2019-04-11`,
+      },
+      {
+        id: `r000016`,
+        text: `No major issues just minor ones of the TV was not a smart TV, so it could connect to our devices wireless and we had to use a cable to stream things from our laptop.`,
+        rating: 4.4,
+        user: {
+          name: `Caroline`,
+        },
+        date: `2019-03-10`,
+      },
+      {
+        id: `r000017`,
+        text: `Very spacious, clean and in a perfect location.`,
+        rating: 4.9,
+        user: {
+          name: `Sandra`,
+        },
+        date: `2019-05-11`,
+      },
+      {
+        id: `r000018`,
+        text: `Great if you want to stay up talking and others do to bed.`,
+        rating: 4.6,
+        user: {
+          name: `Amanda`,
+        },
+        date: `2019-05-07`,
+      },
+    ],
+    offersNearbyIds: [
+      `offer-2`
+    ],
   }
 ];
