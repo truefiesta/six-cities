5753315fe998601f9bb4a6868a4d02f6383f72c5
Yulia Karpova <karpova.jul@gmail.com>
2020-07-24 11:01:04 +1000

    7.14 Создаст компонент «Sign In»

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,66 @@
+import React, {PureComponent, createRef} from "react";
+import PropTypes from "prop-types";
+
+class SignIn extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._loginRef = createRef();
+    this._passwordRef = createRef();
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleSubmit(evt) {
+    const {onSubmit} = this.props;
+    evt.preventDefault();
+
+    onSubmit({
+      login: this._loginRef.current.value,
+      password: this._passwordRef.current.value,
+    });
+  }
+
+  render() {
+    return (
+      <section className="login">
+        <h1 className="login__title">Sign in</h1>
+        <form
+          className="login__form form"
+          action="#"
+          method="post"
+          onSubmit={this._handleSubmit}
+        >
+          <div className="login__input-wrapper form__input-wrapper">
+            <label className="visually-hidden" htmlFor="email">E-mail</label>
+            <input
+              className="login__input form__input"
+              type="email"
+              name="email"
+              placeholder="Email"
+              required=""
+              ref={this._loginRef}
+            />
+          </div>
+          <div className="login__input-wrapper form__input-wrapper">
+            <label className="visually-hidden" htmlFor="password">Password</label>
+            <input
+              className="login__input form__input"
+              type="password"
+              name="password"
+              placeholder="Password"
+              required=""
+              ref={this._passwordRef}
+            />
+          </div>
+          <button className="login__submit form__submit button" type="submit">Sign in</button>
+        </form>
+      </section>
+    );
+  }
+}
+
+SignIn.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default SignIn;
