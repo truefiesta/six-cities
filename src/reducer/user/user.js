96faa5c77c077ad2d99fc48327fb6cb767b83bd3
Yulia Karpova <karpova.jul@gmail.com>
2020-07-24 11:26:20 +1000

    7.15 Создаст файл-редьюсер user.js c operation для проверки состояние пользователя

---
+++ src/reducer/user/user.js
@@ -0,0 +1,48 @@
+import {extend} from "../../utils.js";
+
+const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
+};
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+const ActionType = {
+  REQUIRE_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+};
+
+const ActionCreator = {
+  requireAuthorization: (status) => {
+    return {
+      type: ActionType.REQUIRE_AUTHORIZATION,
+      payload: status,
+    };
+  }
+};
+
+const Operation = {
+  checkAuth: () => (dispatch, getState, api) => {
+    return api.get(`/login`)
+      .then(() => {
+        dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRE_AUTHORIZATION:
+      return extend(state, {
+        authorizationStatus: action.payload,
+      });
+  }
+
+  return state;
+};
+
+export {AuthorizationStatus, ActionType, ActionCreator, Operation, reducer};
