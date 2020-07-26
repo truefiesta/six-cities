a13b74c81119f5f6e1b9718ed5bb225fbcb9bbb2
Yulia Karpova <karpova.jul@gmail.com>
2020-07-26 20:58:21 +1000

    7.31 Обновит тесты для редьюсера filters

--- src/reducer/filters/filters.test.js
+++ src/reducer/filters/filters.test.js
@@ -1,19 +1,36 @@
-import {reducer, ActionCreator, ActionType} from "./filters.js";
+import {reducer, ActionCreator, ActionType, initialState} from "./filters.js";
 import {CityName, SortTypes} from "../../const.js";
 
-describe(`Action creators work correctly`, () => {
-  describe(`Action creator for changing city`, () => {
-    it(`returns payload: Paris, when city changes to Paris`, () => {
-      expect(ActionCreator.changeCity(CityName.PARIS)).toEqual({
+describe(`Action creators`, () => {
+  describe(`action changeCity`, () => {
+    it(`returns correct type and payload`, () => {
+      const city = CityName.COLOGNE;
+
+      expect(ActionCreator.changeCity(city)).toEqual({
         type: ActionType.CHANGE_CITY,
-        payload: CityName.PARIS,
+        payload: city,
       });
     });
+  });
 
-    it(`returns payload: Amsterdam, when city changes to Amsterdam`, () => {
-      expect(ActionCreator.changeCity(CityName.AMSTERDAM)).toEqual({
-        type: ActionType.CHANGE_CITY,
-        payload: CityName.AMSTERDAM,
+  describe(`action changeSortType`, () => {
+    it(`returns correct type and payload`, () => {
+      const sortType = SortTypes.TOP_RATED_FIRST;
+
+      expect(ActionCreator.changeSortType(sortType)).toEqual({
+        type: ActionType.CHANGE_SORT_TYPE,
+        payload: sortType,
+      });
+    });
+  });
+
+  describe(`action changeActiveCard`, () => {
+    it(`returns correct type and payload`, () => {
+      const activeOffer = {offerId: 34};
+
+      expect(ActionCreator.changeActiveCard(activeOffer)).toEqual({
+        type: ActionType.CHANGE_ACTIVE_CARD,
+        payload: activeOffer,
       });
     });
   });
@@ -21,32 +38,42 @@ describe(`Action creators work correctly`, () => {
 
 describe(`Reducer`, () => {
   it(`without additional parameters should return initial state`, () => {
-    expect(reducer(undefined, {})).toEqual({
-      activeCard: null,
-      city: ``,
-      sortType: SortTypes.POPULAR,
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  describe(`action changeCity`, () => {
+    it(`sets new city`, () => {
+      const originalState = Object.assign({}, initialState, {city: CityName.AMSTERDAM});
+      const newCity = CityName.BRUSSELS;
+      const expectedState = Object.assign({}, originalState, {city: newCity});
+
+      expect(
+          reducer(originalState, ActionCreator.changeCity(newCity))
+      ).toEqual(expectedState);
     });
   });
 
-  it(`with no state and action type change city returns city and empty array of offers`, () => {
-    expect(reducer(undefined, ActionCreator.changeCity(CityName.PARIS))).toEqual({
-      activeCard: null,
-      city: CityName.PARIS,
-      sortType: SortTypes.POPULAR,
+  describe(`action changeSortType`, () => {
+    it(`sets new sort type`, () => {
+      const originalState = Object.assign({}, initialState, {sortType: SortTypes.POPULAR});
+      const newSortType = SortTypes.TOP_RATED_FIRST;
+      const expectedState = Object.assign({}, originalState, {sortType: newSortType});
+
+      expect(
+          reducer(originalState, ActionCreator.changeSortType(newSortType))
+      ).toEqual(expectedState);
     });
   });
 
-  it(`with state and action type change city returns new city`, () => {
-    const state = {
-      activeCard: null,
-      city: CityName.AMSTERDAM,
-      sortType: SortTypes.POPULAR,
-    };
+  describe(`action changeActiveCard`, () => {
+    it(`sets new active offer`, () => {
+      const originalState = Object.assign({}, initialState, {activeCard: {offerId: 1}});
+      const newOffer = {offerId: 34};
+      const expectedState = Object.assign({}, originalState, {activeCard: newOffer});
 
-    expect(reducer(state, ActionCreator.changeCity(CityName.PARIS))).toEqual({
-      activeCard: null,
-      city: CityName.PARIS,
-      sortType: SortTypes.POPULAR,
+      expect(
+          reducer(originalState, ActionCreator.changeActiveCard(newOffer))
+      ).toEqual(expectedState);
     });
   });
 });
