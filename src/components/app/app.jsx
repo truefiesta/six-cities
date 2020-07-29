5f7e1a711dfad78511b8f2580ae9b6d6ad1410cd
Yulia Karpova <karpova.jul@gmail.com>
2020-07-29 18:18:20 +1000

    8.12 Создаст PrivateRoute и обернет им FavoritesScreen в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,9 @@ import React, {PureComponent} from "react";
 import {Switch, Route, BrowserRouter as Router} from "react-router-dom";
 import Main from "../main/main.jsx";
 import OfferDetails from "../offer-details/offer-details.jsx";
+import FavoritesScreen from "../favorites-screen/favorites-screen.jsx";
 import LoginScreen from "../login-screen/login-screen.jsx";
+import PrivateRoute from "../private-route/private-route.jsx";
 import {AppRoute} from "../../const.js";
 
 class App extends PureComponent {
@@ -14,6 +16,15 @@ class App extends PureComponent {
           <Route exact path={AppRoute.ROOT} component={Main}/>
           <Route path={AppRoute.LOGIN} component={LoginScreen}/>
           <Route path={AppRoute.ROOM} component={OfferDetails}/>
+          <PrivateRoute
+            exact
+            path={AppRoute.FAVORITE}
+            render={() => {
+              return (
+                <FavoritesScreen/>
+              );
+            }}
+          />
         </Switch>
       </Router>
     );
