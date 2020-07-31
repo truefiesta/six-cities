c77211ea0020297a45098eeac8d1cf95a3bfeb20
Yulia Karpova <karpova.jul@gmail.com>
2020-07-31 19:56:28 +1000

    9.10 Заменит BrowserRouter на Router и установит history версии 4.6.3

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,18 @@
 import React, {PureComponent} from "react";
-import {Switch, Route, BrowserRouter as Router} from "react-router-dom";
+import {Switch, Route, Router} from "react-router-dom";
 import Main from "../main/main.jsx";
 import OfferDetails from "../offer-details/offer-details.jsx";
 import FavoritesScreen from "../favorites-screen/favorites-screen.jsx";
 import LoginScreen from "../login-screen/login-screen.jsx";
 import PrivateRoute from "../private-route/private-route.jsx";
 import {AppRoute} from "../../const.js";
+import history from "../../history.js";
 
 class App extends PureComponent {
   render() {
 
     return (
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route exact path={AppRoute.ROOT} component={Main}/>
           <Route path={AppRoute.LOGIN} component={LoginScreen}/>
