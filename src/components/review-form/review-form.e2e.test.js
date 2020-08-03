438828ba7815751443322261cc32fcf4fb2be947
Yulia Karpova <karpova.jul@gmail.com>
2020-08-03 22:31:10 +1000

    9.28 Добавит и обновит тесты ReviewForm

---
+++ src/components/review-form/review-form.e2e.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Enzyme, {mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {ReviewForm} from "./review-form.jsx";
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+describe(`ReviewForm`, () => {
+  it(`calls onReviewSubmit on click`, () => {
+    const onReviewSubmit = jest.fn();
+
+    const reviewFormComponent = mount(
+        <ReviewForm
+          reviewError={``}
+          review={`Nice and cozy apartment near the city center and with a nice view`}
+          rating={4}
+          isEnabled={false}
+          isBlocked={false}
+          minReviewLength={50}
+          maxReviewLength={300}
+          onRatingChange={() => null}
+          onReviewChange={() => null}
+          onReviewSubmit={onReviewSubmit}
+        />
+    );
+
+    const reviewForm = reviewFormComponent.find(`form`);
+    expect(reviewForm).toHaveLength(1);
+
+    reviewForm.simulate(`submit`, {preventDefault() {}});
+    expect(onReviewSubmit).toHaveBeenCalledTimes(1);
+  });
+
+  it(`calls onReviewChange on click`, () => {
+    const onReviewChange = jest.fn();
+
+    const reviewFormComponent = mount(
+        <ReviewForm
+          reviewError={``}
+          review={``}
+          rating={4}
+          isEnabled={false}
+          isBlocked={false}
+          minReviewLength={50}
+          maxReviewLength={300}
+          onRatingChange={() => null}
+          onReviewChange={onReviewChange}
+          onReviewSubmit={() => null}
+        />
+    );
+
+    const reviewMessage = reviewFormComponent.find(`textarea`);
+    expect(reviewMessage).toHaveLength(1);
+
+    reviewMessage.instance().value = `New Review Text`;
+    reviewMessage.simulate(`change`);
+    expect(onReviewChange).toHaveBeenCalledWith(`New Review Text`);
+  });
+});
--- src/components/review-form/review-form.test.js
+++ src/components/review-form/review-form.test.js
@@ -2,25 +2,24 @@ import React from "react";
 import renderer from "react-test-renderer";
 import {ReviewForm} from "./review-form.jsx";
 
-describe(`ReviewForm`, () => {
-  describe(`when the offers is not empty array`, () => {
-    it(`should render with correctly`, () => {
-      const tree = renderer.create(
-          <ReviewForm
-            reviewError={``}
-            review={`Nice and cozy apartment near the city center and with a nice view`}
-            rating={4}
-            isEnabled={false}
-            minReviewLength={50}
-            maxReviewLength={300}
-            onRatingChange={() => null}
-            onReviewChange={() => null}
-            onReviewSubmit={() => null}
-          />
-      )
-      .toJSON();
+describe(`ReviewForm snapshot`, () => {
+  it(`should render`, () => {
+    const tree = renderer.create(
+        <ReviewForm
+          reviewError={``}
+          review={`Nice and cozy apartment near the city center and with a nice view`}
+          rating={4}
+          isEnabled={false}
+          isBlocked={false}
+          minReviewLength={50}
+          maxReviewLength={300}
+          onRatingChange={() => null}
+          onReviewChange={() => null}
+          onReviewSubmit={() => null}
+        />
+    )
+    .toJSON();
 
-      expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
   });
 });
